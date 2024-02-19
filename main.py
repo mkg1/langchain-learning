1e92536428cc9246db86ee5bc001ac61e3ac99bd
mkg1 <gehring.mg@gmail.com>
2024-02-19 11:57:44 -0500

    Linking up sequential chains

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -18,15 +18,35 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"]
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt = code_prompt,
-    output_key = "code"
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for the following {language} code: \n{code}"
 )
 
-result = code_chain({
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain], 
+    input_variables=["task", "language"],
+    output_variables=["test", "code"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result)
+print(">>>>>>>Generated Code: ")
+print(result["code"])
+print(">>>>>>>>Generated Test: ")
+print(result["test"])
