44207acc421177fcea27b774c24ae73e892288b2
mkg1 <gehring.mg@gmail.com>
2024-03-13 21:26:14 -0400

    Set up memory to allow previous messages to be sent to model along with current

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
 
 # load env vars
 load_dotenv()
@@ -9,16 +10,21 @@ load_dotenv()
 # create instance of chat model
 chat = ChatOpenAI()
 
+# ConversationBufferMemory - useful with conversational models; allows previous messages to be sent along with current message to model.
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True:
