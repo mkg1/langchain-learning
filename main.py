0e0289118d2475e8d03bd8ceb915cdcc1c465fec
mkg1 <gehring.mg@gmail.com>
2024-03-16 19:36:38 -0400

    Initial doc loader setup

--- main.py
+++ main.py
@@ -1,43 +1,12 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import LLMChain
+from langchain.document_loaders import TextLoader
 from dotenv import load_dotenv
-from langchain.memory import FileChatMessageHistory, ConversationSummaryMemory
 
-# load env vars
 load_dotenv()
 
-# create instance of chat model
-chat = ChatOpenAI()  
+# langchain includes various loaderes for different file types, including loading files from amazon s3 buckets
+# langchain file loaders return a document with at least 2 properties: page_content & metadata
 
-# ConversationBufferMemory - useful with conversational models; allows previous messages to be sent along with current message to model.
-# FileChatMessageHistory - allows you to save messages to a file as json for later use
-# ConversationSummaryMemory - uses it's own LLM to summarize the chat to avoid huge chats stacking up (but doesn't play well with FileChatMessageHistory)
-# Adds some performance overhead bc every sent message results in a second step of summarizing the chat.
-memory = ConversationSummaryMemory(
-    # chat_memory=FileChatMessageHistory("messages.json"),
-    memory_key="messages", 
-    return_messages=True,
-    llm=chat,
-    )
+loader = TextLoader("facts.txt")
+docs = loader.load()
 
-prompt = ChatPromptTemplate(
-    input_variables=["content", "messages"],
-    messages=[
-        MessagesPlaceholder(variable_name="messages"),
-        HumanMessagePromptTemplate.from_template("{content}")
-    ]
-)
-
-chain = LLMChain(
-    llm=chat,
-    prompt=prompt,
-    memory=memory
-)
-
-while True:
-    # built in fn - will pause execution and wait for user input + user hits Enter
-    content = input(">> ")
-
-    result = chain({"content": content})
-    print(result["text"])
+print(docs)
